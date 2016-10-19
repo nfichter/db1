74107b2799d552a6d35f692a00fbbc7a6d2c5898
Noah Fichter <nfichter@stuy.edu>
2016-10-19 13:31:17 -0400

    finished, but strings in unicode

--- basic.py
+++ basic.py
@@ -5,8 +5,24 @@ c = db.cursor()
 
 c.execute("SELECT name,mark,courses.id FROM courses,peeps WHERE courses.id = peeps.id")
 
+fetched = c.fetchall()
+
 d = {}
 
-for row in c.fetchall():
-    grades = []
-    d[row[2]] = [[row[0],row[2]],grades]
+for row in fetched:
+    d[row[2]] = [row[0],row[2]]
+
+for key in d.keys():
+    sumGrades = 0
+    numGrades = 0
+    for row in fetched:
+        if key == row[2]:
+            sumGrades += row[1]
+            numGrades += 1
+    average = (sumGrades*1.0)/numGrades
+    rightnow = d[key]
+    rightnow.append(average)
+    d[key] = rightnow
+
+for key in d.keys():
+    print d[key]
